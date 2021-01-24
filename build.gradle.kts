3638ebffa000ff40139abf5e2bf6136fe0667793
sya-ri <sya79lua@gmail.com>
2021-01-24 22:32:37 +0900

    chore/*: Ktlintを導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,19 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
 plugins {
     id(GradlePlugin.kotlin_jvm) version Version.kotlin
+    id(GradlePlugin.ktlint_gradle) version Version.ktlint_gradle
 }
 
 allprojects {
+    apply(plugin = GradlePlugin.ktlint_gradle)
+
     repositories {
         mavenCentral()
+    }
+
+    configure<KtlintExtension> {
+        version.set(Version.ktlint)
     }
 }
 
--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,4 +1,6 @@
 object Version {
+    const val ktlint = "0.40.0"
+    const val ktlint_gradle = "9.4.1"
     const val kotlin = "1.4.21"
     const val ktor = "1.5.0"
     const val kotlin_css_jvm = "1.0.0-pre.138-kotlin-1.4.21"
